8b95144f514779d86f73223ad4019bfffc833e7c
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-09-03 19:23:18 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 const navbarNav = document.querySelector('.navbar-nav');
-document.querySelector('a[href="#Menu"]').addEventListener('click', function(eventScrollKeBawah) {
-    eventScrollKeBawah.preventDefault();
-    const TPkeBagian = document.getElementById('Menu');
-    TPkeBagian.scrollIntoView({ behavior: 'smooth' });
+
+// Navigation Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
 });
 
+
+
 // Scroll To Top button
+// script.js
 window.onscroll = function () {
     scrollFunction();
 };
@@ -21,6 +31,11 @@ function scrollFunction() {
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    // Menggunakan requestAnimationFrame() untuk animasi scroll
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentScroll - currentScroll / 5); // Mengurangi perlahan scroll sebelum mencapai atas
+    }
 }
